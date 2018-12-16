69ec73858c8ec7501c06972d090e9e49a2fa5356
jeremyy55 <jeremy-y@live.be>
2018-12-16 13:22:22 +0100

    Updates

---
+++ precache-manifest.ed1994e7d3f890ed505fb7583661eba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-umons-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5cedfc5f144ed236c934",
+    "url": "/react-umons-project/static/js/runtime~main.5cedfc5f.js"
+  },
+  {
+    "revision": "32884b608e4708193571",
+    "url": "/react-umons-project/static/js/main.32884b60.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-umons-project/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "32884b608e4708193571",
+    "url": "/react-umons-project/static/css/main.e17a6f48.chunk.css"
+  },
+  {
+    "revision": "b42cfcb93630875494a8fa9737d68d93",
+    "url": "/react-umons-project/index.html"
+  }
+];
