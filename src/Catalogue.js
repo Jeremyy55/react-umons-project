7683a616d9cfb50125b9664b224e01217e442d93
WolfMister54 <pierre.tondeur54@gmail.com>
2019-01-17 05:04:04 +0100

    ajout de composant fontend Catalogue

---
+++ src/Catalogue.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Grid, Row, Col, Thumbnail, Carousel, ListGroup, ListGroupItem, PageHeader} from "react-bootstrap";
+import "react-bootstrap";
+import carousel from "./carousel.png"
+import ReactBootstrapCarousel from "react-bootstrap-carousel";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+
+
+
+const catalogue = [
+    { title: 'kot 1', description: 'très bel appartement', price: '444', adresse: 'rue de la houille numéro 54', owner: 1 },
+    { title: 'kot 2', description: 'très bel appartement', price: '444', adresse: 'rue de la houille numéro 54', owner: 1 },
+    ]
+
+
+const images = [{path : carousel}, {path : carousel},]
+
+
+const Catalogue = ({}) => (
+  <p>
+        <PageHeader>
+          Catalogue
+        </PageHeader>
+
+        <Grid>
+          <Row>
+            <Col xs={8}>
+              <ListGroup>
+                <div className="Catalogue">
+                  {catalogue.map(({title, description, price, adresse, owner}) => (
+                  <ListGroupItem header={title}>
+                    
+                    <Carousel className="Images">
+                      {images.map(({}) => (
+                        <Carousel.Item>
+                          <img width={600} height={300} alt="400x250" src={carousel} />
+
+                           <Carousel.Caption>
+                             <h3>First slide label</h3>
+                             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                           </Carousel.Caption>
+                        </Carousel.Item>
+                      ))}
+                    </Carousel>
+                    
+                    <p>Description : {description}</p>
+
+                    <p>Prix : {price}</p>
+
+                    <p>Adresse : {adresse}</p>
+
+                    <p>Nombre de personnes : {owner}</p>
+
+                    <p>
+                      <Button bsStyle="success">louer</Button>
+                    </p>
+                  </ListGroupItem>
+                  ))}
+                </div>
+              </ListGroup>
+            </Col>
+          </Row>
+        </Grid>
+      </p>
+)
+
+export default Catalogue
